8c8fa6167e29ff41f0fb09b99d701ed2bdb0142e
ranjak <ranjak.hector@gmail.com>
2017-01-25 15:34:52 +0100

    Move server logic into its own class.

---
+++ StickyPlatformsServer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stormancer;
+using Stormancer.Core;
+using Stormancer.Plugins;
+
+namespace stickyplatforms_server
+{
+  class StickyPlatformsServer
+  {
+    private struct Color
+    {
+      public byte r, g, b, a;
+
+      public Color(byte r, byte g, byte b, byte a = 255)
+      {
+        this.r = r; this.g = g; this.b = b; this.a = a;
+      }
+    }
+
+    private struct Vector2
+    {
+      public float x, y;
+
+      public Vector2(float x, float y)
+      {
+        this.x = x;
+        this.y = y;
+      }
+    }
+
+    private class Player
+    {
+      public string name;
+      public Color color;
+      public Vector2 position;
+      public Vector2 velocity;
+      public int hp;
+
+      public Player(string name)
+      {
+        this.name = name;
+      }
+    }
+
+    // Map file for the scene
+    private string mMapFilename;
+    // Peer ID => Player
+    private Dictionary<long, Player> mPlayers;
+
+    Task onConnecting(IScenePeerClient client)
+    {
+      // We need unique names
+      string name = client.GetUserData<string>();
+
+      foreach (KeyValuePair<long, Player> player in mPlayers)
+      {
+        if (player.Value.name == name)
+        {
+          throw new ClientException("The username you chose (\"" + name + "\") is already in use.");
+        }
+      }
+
+      return Task.FromResult(true);
+    }
+
+    Task onConnected(IScenePeerClient client)
+    {
+      string name = client.GetUserData<string>();
+      mPlayers[client.Id] = new Player(name);
+
+      return Task.FromResult(true);
+    }
+
+    Task getMapProcedure(RequestContext<IScenePeerClient> ctx)
+    {
+      ctx.SendValue(mMapFilename);
+      return Task.FromResult(true);
+    }
+
+    public StickyPlatformsServer(ISceneHost scene)
+    {
+      scene.Connecting.Add(onConnecting);
+      scene.Connected.Add(onConnected);
+      scene.AddProcedure("getMapFilename", getMapProcedure);
+    }
+  }
+}
--- app.cs
+++ app.cs
@@ -11,73 +11,14 @@ namespace stickyplatforms_server
 {
   public class App
   {
-    private struct Color
-    {
-      public byte r, g, b, a;
-
-      public Color(byte r, byte g, byte b, byte a=0)
-      {
-        this.r = r; this.g = g; this.b = b; this.a = a;
-      }
-    }
-
-    private struct Vector2
-    {
-      public float x, y;
-
-      public Vector2(float x, float y)
-      {
-        this.x = x;
-        this.y = y;
-      }
-    }
-
-    private class Player
-    {
-      public string name;
-      public Color color;
-      public Vector2 position;
-      public Vector2 velocity;
-      public int hp;
-
-      public Player(string name)
-      {
-        this.name = name;
-      }
-    }
-
     public void Run(IAppBuilder builder)
     {
       builder.SceneTemplate("gameSession", scene =>
       {
-        scene.Connecting.Add(client =>
-        {
-          // We need unique names
-          string name = client.GetUserData<string>();
-
-          foreach (KeyValuePair<long, Player> player in mPlayers) {
-            if (player.Value.name == name) {
-              throw new ClientException("The username you chose (\"" + name + "\") is already in use.");
-            }
-          }
-
-          return Task.FromResult(true);
-        });
-
-        scene.Connected.Add(client =>
-        {
-          string name = client.GetUserData<string>();
-          mPlayers[client.Id] = new Player(name);
-
-          return Task.FromResult(true);
-        });
-
+        new StickyPlatformsServer(scene);
       });
     }
 
-    // Peer ID => Player
-    private Dictionary<long, Player> mPlayers;
-    private string mMapFilename;
   }
 
 }
